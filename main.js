d3c5bb4ceab5a5e8fb74d4942ea16bd7effb2a8d
niwei822 <cecily.li.822@gmail.com>
2024-06-29 23:01:56 -0700

    completed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css';
 import getRandomWord from './src/randomWord.js';
 import setSharkImage from './src/sharkImage.js';
 import { isLetterInWord, revealLetterInWord, setupWord } from './src/word';
-
+import setupGuesses from './src/guess.js';
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -18,9 +18,39 @@ function initSharkwords() {
   
   let numWrong = 0;
   const word = getRandomWord();
-  setSharkImage(document.querySelector('#shark-img'), numWrong);
-  setupWord(document.querySelector('#word-container'), word);
+  const gameStatusEl = document.querySelector('#game-status');
+  const wordContainerEl = document.querySelector('#word-container');
+  const sharkImageEl = document.querySelector('#shark-img');
+  const letterButtonEl = document.querySelector('#letter-buttons');
+
+  setSharkImage(sharkImageEl, numWrong);
+  setupWord(wordContainerEl, word);
+
+  const handleGuess = (guessEvent, letter) => {
+   const button = guessEvent.target;
+   button.disabled = true;
+    if(isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong++;
+      setSharkImage(sharkImageEl, numWrong);
+    }
+
+    if(numWrong === 5) {
+      gameStatusEl.innerHTML = `You lose! The word was: ${word}`;
+      disableAllButtons();
+    } else if(word.split('').every((i) => i === letter)) {
+      gameStatusEl.innerHTML = `You win! The word was: ${word}`;
+      disableAllButtons();
+    }
+
+    function disableAllButtons() {
+      const buttons = document.querySelectorAll('button');
+      buttons.forEach((i) => i.disabled = true);
+    }
+  };
   
+  setupGuesses(letterButtonEl, handleGuess);
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
--- src/guess.js
+++ src/guess.js
@@ -1,10 +1,23 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
+/**
+ * Set up the guess buttons and attach event listeners to them.
+ * 
+ * @param {HTMLElement} element - The element to which the buttons will be appended.
+ * @param {Function} handleGuess - The function that will be called when a guess is made.
+ */
 function setupGuesses(element, handleGuess) {
+  // Split the alphabet string into an array of individual letters.
   alphabet.split('').forEach((letter) => {
+    // Create a new button element for each letter.
     const btn = document.createElement('button');
-    btn.innerText = letter;
+    btn.innerText = letter; // Set the text content of the button to the letter.
+    
+    // Attach an event listener to the button that calls the handleGuess function
+    // with the letter as an argument.
     btn.addEventListener('click', (e) => handleGuess(e, letter));
+    
+    // Append the button to the specified element.
     element.append(btn);
   });
 }
